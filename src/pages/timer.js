456e4855a63b6d9249b67919ef6f0b52d266a737
n0z0me <n0z0me.2ca@gmail.com>
2021-06-24 23:51:44 +0900

    add: next time that forwarded 15 mins from now time

--- src/pages/timer.js
+++ src/pages/timer.js
@@ -4,8 +4,35 @@ import Head from 'next/head'
 import Footer from '../components/Footer'
 import Header from "../components/Header"
 import styles from '../styles/Timer.module.css'
+import { faSortNumericDown } from "@fortawesome/free-solid-svg-icons"
+import { useEffect, useState } from 'react'
 
 export default function Timer() {
+  let now = new Date();
+
+  const timeStr = date => `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  const nextTiming = () => 1000 - Date.now() % 1000;
+
+  const [nowTime, setNowTime] = useState(
+    () => timeStr(new Date)
+  );
+  
+  const [nextTime, setNextTime] = useState(
+    () => timeStr(new Date)
+  );
+  
+  useEffect(() => {
+    let timer = setTimeout(function main(){
+      now = new Date();
+      next = new Date(now.getTime() + 15 * 60000);
+      timer = setTimeout(main, nextTiming());
+      setNowTime(timeStr(now));
+      setNextTime(timeStr(next));
+    }, nextTiming());
+    return () => clearTimeout(timer);
+  }, [setNowTime, setNextTime]);
+
+
   return (
     <div className={styles.container}>
       <Head>
@@ -14,8 +41,8 @@ export default function Timer() {
       </Head>
 
       <main className={styles.main}>
-        <div>現在時刻</div>
-        <div>15分後</div>
+        <div>{nowTime}</div>
+        <div>{nextTime}</div>
       </main>
     </div>
   )
